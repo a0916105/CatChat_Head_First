19278946201f054c2f0e697fdd524468f88c304a
a0916105 <whdofjew@gmail.com>
2022-12-20 16:21:07 +0800

    讓工具列視為預設的app bar，才會顯示app的名稱

--- app/src/main/java/tw/idv/jew/catchat/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/catchat/MainActivity.kt
@@ -2,10 +2,15 @@ package tw.idv.jew.catchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //讓工具列視為預設的app bar，才會顯示app的名稱
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
