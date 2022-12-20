ee60335909738d9b777b19f035ef7b7aef0d8bea
a0916105 <whdofjew@gmail.com>
2022-12-20 21:24:39 +0800

    menu_toolbar

--- app/src/main/java/tw/idv/jew/catchat/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/catchat/MainActivity.kt
@@ -2,6 +2,13 @@ package tw.idv.jew.catchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +19,29 @@ class MainActivity : AppCompatActivity() {
         //讓工具列視為預設的app bar，才會顯示app的名稱
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        //設置工具列來加入Up按鈕並顯示你在哪個畫面
+        //從瀏覽容器取得navController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        //建立appBarConfiguration，將工具列接到導覽圖
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builder.build()
+        //讓工具列使用appBarConfiguration
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    //將menu_toolbar的選單資源檔裡面的所有項目加入工具列
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    //當工具列裡的項目被按下時，NavController會前往正確的destination
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController)
+                || super.onOptionsItemSelected(item)
     }
 }
