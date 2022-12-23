6df46b2fc88cf8627fd13b5aa96f6ceffeac5d49
a0916105 <whdofjew@gmail.com>
2022-12-23 10:33:40 +0800

    drawer(no group)

--- app/src/main/java/tw/idv/jew/catchat/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/catchat/MainActivity.kt
@@ -1,16 +1,18 @@
 package tw.idv.jew.catchat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,11 +28,20 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        //取得抽屜的參考
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+
         //建立appBarConfiguration，將工具列接到導覽圖
         val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawer)   //將抽屜加入AppBarConfiguration
         val appBarConfiguration = builder.build()
         //讓工具列使用appBarConfiguration
         toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        //將抽屜接到導覽控制器，讓按下項目時前往ID相符的地方
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        NavigationUI.setupWithNavController(navView, navController)
     }
 
     //將menu_toolbar的選單資源檔裡面的所有項目加入工具列
